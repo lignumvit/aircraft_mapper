378cad6b3ca3d9d47a622b8878dfa1a3fdbfaf4a
Christopher Kruse <ckruse@ucar.edu>
2024-10-22 16:44:15 -0600

    Added tests of the flight data reader.

--- test/test_utils.py
+++ test/test_utils.py
@@ -2,18 +2,41 @@ import sys
 sys.path.append('..')
 import util.utils as utils
 import pytest
+import numpy as np
+from datetime import datetime
 
 def test_open_nc():
     # test bad file path, check that FileNotFoundError results
     file_path = "./test_fligt.nc"
     with pytest.raises(FileNotFoundError):
-        utils.open_nc(file_path)
+        utils.open_flight_nc(file_path)
 
     # now test where file exists
     file_path = "./test_flight.nc"
-    assert utils.open_nc(file_path)
+    assert utils.open_flight_nc(file_path)
     
 def test_read_nc():
     file_path = "./test_flight.nc"
-    nc = utils.open_nc(file_path)
+    nc = utils.open_flight_nc(file_path)
 
+    vars_to_read = ['Time','GGALT','LATC','LONC','UIC','VIC','WIC']
+    df = utils.read_flight_nc(nc, vars_to_read) # only tests that no errors were thrown, so not a great test
+
+def test_sfm_to_datetime():
+    # a test array of sfm (seconds-from-midnight) with units of tunits
+    sfm = np.array([0,60,3600.5])
+    tunits = 'seconds since 2018-01-15 00:00:00 +0000'
+
+    # call the function
+    dt_list = utils.sfm_to_datetime(sfm,tunits)
+
+    # define the expected result
+    expected_list = [datetime.fromisoformat('2018-01-15T00:00:00+00:00'),
+                     datetime.fromisoformat('2018-01-15T00:01:00+00:00'),
+                     datetime.fromisoformat('2018-01-15T01:00:00.5+00:00')]
+    # compare
+    for (test, expected) in zip(dt_list, expected_list):
+        assert test == expected
+
+if __name__ == "__main__":
+    test_read_nc()
--- util/utils.py
+++ util/utils.py
@@ -1,20 +1,42 @@
 import netCDF4
 import pathlib as path
+import pandas as pd
+import numpy as np
+from datetime import datetime, timedelta
 
-read_vars = ['ADIFR', 'BDIFR', 'ADIFRTEMP', 'BDIFRTEMP', # adifr, bdifr
-             'QCF', 'QCFR', 'QCR', 'QC_A', 'QC_A2', 'QCFRC',      # raw, dynamic pressures (q)
-             'GGLAT','GGLON','GGNSAT','GGQUAL','GGSPD','GGTRK', # GPS variables
-             'PSFD', 'PSFRD', 'PSXC', # static pressures
-             'VEW', 'VNS', 'VSPD', 'GGVEW', 'GGVNS', 'GGVSPD', 'VEWC', 'VNSC', # aircraft velocities, raw and blended
-             'UI', 'UIC', 'VI', 'VIC', 'WI', 'WIC', # winds, both uncorrected and GPS-corrected
-             'PALTF', 'PALT', 'GGALT', 'ALT', # altitudes
-             'TASF', 'TASFR', 'TASR', 'TAS_A', 'TAS_A2', 'MACHX', # speeds
-             'PITCH', 'ROLL', 'THDG', # attitude
-             'AKRD', 'SSLIP', # flow angles
-             'RHUM', 'RICE', 'ATX', 'BNORMA', 'BLATA', 'BLONGA', 'WDC',
-            ]
+# vars_to_read is the default set of variables that get read when calling read_nc below when
+# variables are not specified (i.e. when called like "utils.read_nc(netcdf_obj)"). 
+vars_to_read = ['Time','GGALT','LATC','LONC', # 4-D Position
+                'UIC','VIC','WIC',            # winds
+                'ATX','PSFC','EWX',           # other state params
+               ]
 
-def open_nc(file_path: str) -> netCDF4._netCDF4.Dataset:
+def sfm_to_datetime(sfm, tunits: str) -> list[datetime]:
+    """
+    sfm_to_datetime converts an iterable of seconds from midnight with units of tunits to a list
+
+    :param sfm: An iterable/array of times with units of seconds.
+    :param tunits: A string defining the units of sfm. Expected to be in the form of "seconds from YYYY-MM-DD HH:mm:SS +HHMM"
+
+    :return: Returns a list of Python datetimes.
+    """
+
+    deltas = np.array([timedelta(seconds=s) for s in sfm])
+    tunits_split = tunits.split(' ')
+    t0_iso_str = tunits_split[2]+"T"+tunits_split[3]+tunits_split[4]
+    t0_dt = datetime.fromisoformat(t0_iso_str)
+    
+    dts = [t0_dt + delta for delta in deltas]
+    return dts
+
+def open_flight_nc(file_path: str) -> netCDF4._netCDF4.Dataset:
+    """
+    open_flight_nc simply checks to see if the file at the provided path string exists and opens it.
+
+    :param file_path: A path string to a flight data file, e.g. "./test/test_flight.nc"
+
+    :return: Returns netCDF4._netCDF4.Dataset object.
+    """
 
     fp_path = path.Path(file_path)
     if not fp_path.is_file():
@@ -22,45 +44,59 @@ def open_nc(file_path: str) -> netCDF4._netCDF4.Dataset:
 
     return netCDF4.Dataset(file_path)
 
-def read_nc(nc: netCDF4._netCDF4.Dataset):
-    # sometimes the netcdf4 api produces an issue with big-endian buffer on little-endian compiler
-    byte_swap = False
-    
-    # create empty placeholders for asc, histo_asc and units
-    data = {}
-    units = {}
-    
-    # use the netcdf4 api to get the netcdf data into a dataframe
-#    try:
-        
-    # loop over keys in netCDF file and organize
-    #for i in nc.variables.keys():
-    for i in read_vars:
-        try:
-            output = nc[i][:]
-            data[i] = pd.DataFrame(output)
-            units_var = nc.variables[i].getncattr('units')
-            units[i] = pd.Series(units_var)
-            data[i].columns = pd.MultiIndex.from_tuples(zip(data[i].columns, units[i]))
+def read_flight_nc(nc: netCDF4._netCDF4.Dataset, read_vars: list[str] = vars_to_read) -> pd.DataFrame:
+    """
+    read_flight_nc reads a set of variables into memory.
 
-        except Exception as e:
-            print(e)
+    NOTE: a high-rate, usually 25 Hz, flight data file is assumed.
 
-    # add times
-    i = 'Time'
-    output = nc[i][:]
-    data[i] = pd.DataFrame(output)
-    units_var = nc.variables[i].getncattr('units')
-    units[i] = pd.Series(units_var)
-    data[i].columns = pd.MultiIndex.from_tuples(zip(data[i].columns, units[i]))
+    :param nc: netCDF4._netCDF4.Dataset object opened by open_flight_nc.
+    :param read_vars: An optional list of strings of variable names to be read into memory. A default
+                      list, vars_to_read, is specified above. Passing in a similar list will read in those variables
+                      instead.
 
-    # concatenate the dataframe
+    :return: Returns either a dictionary of numpy time series arrays or a single pandas data frame.
+    """
+
+    data = [] # an empty list to accumulate Dataframes of each variable to be read in
+
+    hz = 25
+    sub_seconds = np.arange(0,25,1)/25.
+
+    for var in read_vars:
+        if var == "Time":
+            # time is provided every second, so need to calculate 25 Hz times efficiently
+            tunits = getattr(nc[var],'units')
+            time = nc[var][:]
+
+            time_25hz = np.zeros((len(time),hz)) # 2-D
+            for i,inc in enumerate(sub_seconds):
+                time_25hz[:,i] = time + inc
+            output = np.ravel(time_25hz) # ravel to 1-D
+            data.append(pd.DataFrame({var: output}))
+            dt_list = sfm_to_datetime(output, tunits)
+            data.append(pd.DataFrame({'datetime': dt_list}))
+        else:
+            ndims = len(np.shape(nc[var][:]))
+            if ndims == 2:
+                # 2-D, 25 Hz variables can just be raveled into 1-D time series
+                output = np.ravel(nc[var][:])
+                data.append(pd.DataFrame({var: output}))
+            elif ndims == 1:
+                # 1-D variables in 25 Hz data files exist (e.g. GGALT is sampled at 20 Hz, but by default,
+                # this is filtered to 1Hz instead of fudged to 25 Hz). Do interpolation to 25 Hz so all time series
+                # have same length.
+                output_1d = nc[var][:]
+                output_2d = np.zeros((len(output_1d),hz))*float("NaN")
+                for i in range(len(output_1d)-1):
+                    output_2d[i,:] = output_1d[i] + sub_seconds*(output_1d[i+1]-output_1d[i]) # divide by 1s omitted
+                output = np.ravel(output_2d)
+                data.append(pd.DataFrame({var: output}))
+            else:
+                raise RuntimeError(f"Variable {var} is {ndims}-dimensional. Only 1-D or 2-D variables are handled.")
+              
+
+    # concatenate the list of dataframes into a single dataframe
     data = pd.concat(data, axis=1, ignore_index=False)
-    # clean up the dataframe by dropping some of the multi-index rows
-    data.columns = data.columns.droplevel(1)
-    data.columns = data.columns.droplevel(1)
-
-    # add a datetime-type time as well
-    data['datetime'] = [timedelta(seconds=int(time)) for time in data['Time']]
 
     return data
