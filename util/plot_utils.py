d75a7f0ecad1051b2372ec29f982b3f890c02d0e
Christopher Kruse <ckruse@ucar.edu>
2024-10-30 11:11:43 -0600

    first version with successful map plotting of field campaigns from netcdf

---
+++ util/plot_utils.py
@@ -0,0 +1,69 @@
+import pandas as pd
+import numpy as np
+import itertools
+from bokeh.io import push_notebook, show, output_notebook
+from bokeh.plotting import figure, show
+from bokeh.models import Title
+from bokeh.palettes import Category10
+
+def plot_track(df: pd.DataFrame, mask: pd.Series = None, title: str =''):
+    if mask is None:
+        mask = np.ones(len(df))
+    # get latitude and longitude from dataframe
+    latitude = df["LATC"].to_numpy().squeeze()
+    longitude = df["LONC"].to_numpy().squeeze()
+    
+    # update to mercator projection
+    k = 6378137
+    longitude = longitude * (k * np.pi/180.0)
+    latitude = np.log(np.tan((90 + latitude) * np.pi/360.0)) * k
+    
+    # create the plot layout and add axis labels
+    try:
+        plot = figure(width=600, height=600, title=title, x_axis_type="mercator", y_axis_type="mercator") 
+        plot.add_layout(Title(text="Longitude [Degrees]", align="center"), "below")
+        plot.add_layout(Title(text="Latitude [Degrees]", align="center"), "left")
+        
+        # add the flight track in yellow and add Esri World Imagery as the basemap
+        # try:
+        if sum(mask) != len(latitude):
+            # condition = np.logical_and(data[flight]["Time"].to_numpy() > start,
+            #                            data[flight]["Time"].to_numpy() < end)
+            lat = df["GGLAT"][mask].to_numpy().squeeze()
+            lon = df["GGLON"][mask].to_numpy().squeeze()
+            lon = lon * (k * np.pi/180.0)
+            lat = np.log(np.tan((90 + lat) * np.pi/360.0)) * k
+            plot.multi_line([longitude,lon],[latitude,lat], color=["yellow","red"])
+        else:
+            plot.line(longitude,latitude, color="yellow")
+    
+        plot.add_tile("Esri World Imagery", retina=True)
+        show(plot)
+    except Exception as e:
+        print(e)
+
+def plot_campaigns(all_campaign_dfs: dict[str,dict[str,pd.DataFrame]]):
+
+    campaign = list(all_campaign_dfs.keys())[0]
+
+    plot = figure(width=900, height=600, x_axis_type="mercator", y_axis_type="mercator") 
+    plot.add_layout(Title(text="Longitude [Degrees]", align="center"), "below")
+    plot.add_layout(Title(text="Latitude [Degrees]", align="center"), "left")
+
+
+    colors = itertools.cycle(Category10[8])
+    for campaign in all_campaign_dfs.keys():
+        color=next(colors)
+        for flight, df in all_campaign_dfs[campaign].items():
+            latitude = df["LATC"].to_numpy().squeeze()
+            longitude = df["LONC"].to_numpy().squeeze()
+            k = 6378137
+            longitude = longitude * (k * np.pi/180.0)
+            latitude = np.log(np.tan((90 + latitude) * np.pi/360.0)) * k
+            
+            plot.line(longitude,latitude, color=color, legend_label=campaign)
+
+    #plot.add_tile("Esri World Imagery", retina=True)
+    plot.add_tile("CartoDB Positron", retina=True)
+
+    show(plot)
