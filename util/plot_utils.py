386fb2ad46d1f7c9d1b07a8657866571502409c2
Christopher Kruse <ckruse@ucar.edu>
2024-11-07 09:25:03 -0700

    updated conda env, cleaned up plotting a bit

--- util/plot_utils.py
+++ util/plot_utils.py
@@ -55,32 +55,45 @@ def plot_campaigns(all_campaign_dfs: dict[str,dict[str,pd.DataFrame]], show_plot
     for campaign in all_campaign_dfs.keys():
         color=next(colors)
         for flight, df in all_campaign_dfs[campaign].items():
-            if "LATC" in list(df.keys()):
-                latitude = df["LATC"].to_numpy().squeeze()
-                longitude = df["LONC"].to_numpy().squeeze()
-            elif "GGLAT" in list(df.keys()):
+            # If GGLAT was read in, use it. Otherwise, default to GPS-corrected IRU lat/lon.
+            # NOTE: GPS-corrected IRU lat/lon has problems when flights cross the dateline.
+            if "GGLAT" in list(df.keys()):
                 latitude = df["GGLAT"].to_numpy().squeeze()
                 longitude = df["GGLON"].to_numpy().squeeze()
-            if np.any(longitude > 175) and np.any(longitude < 175):
+            elif "LATC" in list(df.keys()):
+                latitude = df["LATC"].to_numpy().squeeze()
+                longitude = df["LONC"].to_numpy().squeeze()
+            # To prevent wrap around, detect when a flight crossed the dateline and unwrap the longitudes.
+            #    That is, when lats go -178, -179, 180, 179, 178, ..., and most flights on east of the dateline
+            #    (negative) make all longitudes east of the dateline (negative)
+            if np.any(longitude > 179) and np.any(longitude < -179):
                 num_pos = np.sum(longitude > 0)
                 num_neg = np.sum(longitude < 0)
                 if num_pos >= num_neg:
                     longitude = np.where(longitude < 0, longitude + 360, longitude)
                 else:
                     longitude = np.where(longitude > 0, longitude - 360, longitude)
+            #  ARISTO2017 only had high-rate data, with a handful of lat/lons of 0,0
+            if campaign == "ARISTO2017":
+                mask = np.argwhere(latitude < 20)
+                latitude = np.delete(latitude,mask)
+                longitude = np.delete(longitude,mask)
+            # shift flights so that the wrap point is where we haven't flown (e.g. longitude of +75)
+            #wrap_longitude = 75.0
+            #longitude = np.where(longitude > wrap_longitude, longitude - 360, longitude)
+            # now, convert lat/lon into plot coordinates
             k = 6378137
             longitude = longitude * (k * np.pi/180.0)
             latitude = np.log(np.tan((90 + latitude) * np.pi/360.0)) * k
-            
+            # make that plot
             plot.line(longitude,latitude, color=color, legend_label=campaign)
 
-    #plot.add_tile("Esri World Imagery", retina=True)
-    #plot.legend.location = 'right'
     plot.legend.click_policy = 'hide'
     plot.legend.label_text_font_size = '10px'
     plot.legend.ncols = 2
     plot.add_tile("CartoDB Positron", retina=True)
     plot.add_layout(plot.legend[0],'right')
+    #plot.add_tile("Esri World Imagery", retina=True) # uncomment to add satellite imagery
 
     if show_plot:
         show(plot)
