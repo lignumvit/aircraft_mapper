4e0351f18d8209aa5d2d92eb5d1804dcc4178f2b
Christopher Kruse <ckruse@ucar.edu>
2024-11-11 09:59:43 -0700

    added zi calculation plus some basic thermodynamic helper functions

--- test/test_model_utils.py
+++ test/test_model_utils.py
@@ -16,8 +16,6 @@ from matplotlib.gridspec import GridSpec
 from matplotlib.lines import Line2D
 from mpl_toolkits.axes_grid1 import make_axes_locatable
 
-
-
 cam_fname = './test_ll.nc'
 
 def test_calc_pressure():
@@ -55,5 +53,58 @@ def test_calc_pressure():
     dp = pi[:,1:nlevi,:,:] - pi[:,0:nlevi-1,:,:]
     assert np.all(dp < 0)
 
+def test_calc_phii():
+    ds = xr.open_dataset(cam_fname)
+
+    g = 9.8067
+    phis = ds.PHIS.values/g
+    z = ds.Z3.values[:,::-1,:,:]
+
+    zi_interp = utils.calc_phii_midpoint(z,phis)
+
+    nz = zi_interp.shape[1]
+    dz = zi_interp[:,1:,:,:]-zi_interp[:,0:nz-1,:,:]
+    assert np.all(dz > 0)
+
+def test_calc_phii_hydro():
+    ds = xr.open_dataset(cam_fname)
+
+    # compute zi (z at interfaces) via interpolation, extrapolation
+    g = 9.8067
+    zs = ds.PHIS.values[0,:,:]/g
+    z = ds.Z3.values[:,::-1,:,:]
+    zi_interp = utils.calc_phii_midpoint(z,zs)
+
+    # compute zi (z at interfaces) via hydrostatic equation
+    # read in p, both at grid-cell centers and at vertical interfaces
+    ps = ds.PS.values
+    p = utils.calc_pressure_cesm(ps, ds.P0.values, ds.hyam.values, ds.hybm.values)
+    pi = utils.calc_pressure_cesm(ps, ds.P0.values, ds.hyai.values, ds.hybi.values)
+    # read in the rest of the thermodynamic state variables
+    tk = ds.T.values[:,::-1,:,:]
+    q = ds.Q.values[:,::-1,:,:]
+    # compute the density of moist air given specific humidity
+    rhom = utils.calc_rhom(p,tk,q)
+    # now, calculate heights of vertical iterfaces via hydrostatic integration
+    zi_hydro = utils.calc_phii_hydro(zs,rhom,pi)
+
+    for zi in range(zi_hydro.shape[1]):
+        # prints out a table of index, zi_interp, zi_hydro, and relative difference
+        print(f"{zi:2d} {zi_interp[0,zi,100,100]:8.2f} {zi_hydro[0,zi,100,100]:8.2f} " \
+              f"{(zi_interp[0,zi,100,100]-zi_hydro[0,zi,100,100])/zi_hydro[0,zi,100,100]*100:5.2f}")
+
+    # See how close the two calcs of interface heights are. Want to calculate relative differences, so need to
+    # only consider points where zi_interp == zi_hydro == zs != 0
+    zi_interp = zi_interp.flatten()
+    zi_hydro = zi_hydro.flatten()
+    non_zero_mask = zi_interp != 0
+    ratio = (zi_interp[non_zero_mask]-zi_hydro[non_zero_mask])/zi_hydro[non_zero_mask]*100
+
+    print(f"{np.min(ratio)} {np.max(ratio)} {np.mean(ratio)} {np.std(ratio)}")
+
+    assert np.mean(ratio) < 0.69 # interp method had a relative bias of 0.681 %
+    assert np.std(ratio) < 1.02  # standard deviation between methods of 1.013 %
+    assert np.all(ratio < 6.4)   # the worst single difference between methods was 6.372 %
+
 if __name__ == "__main__":
-    test_calc_pressure()
+    test_calc_phii_hydro()
--- util/model_utils.py
+++ util/model_utils.py
@@ -7,7 +7,12 @@ from datetime import datetime, timedelta
 from fnmatch import fnmatch
 from typing import Iterable
 
-def calc_pressure_cesm(ps: np.ndarray, p0: float, a: np.array, b: np.array) -> list[datetime]:
+g = 9.8067
+Rd = 287.1
+Rv = 461.5
+ep = Rd/Rv
+
+def calc_pressure_cesm(ps: np.ndarray, p0: float, a: np.array, b: np.array) -> np.ndarray:
     """
     calc_pressure_cesm computes pressure on the model levels given surface pressure, reference pressure, and the hybrid
                        level coefficients a and b. See overview at 
@@ -19,8 +24,7 @@ def calc_pressure_cesm(ps: np.ndarray, p0: float, a: np.array, b: np.array) -> l
     :param a: An array of a hybrid level coefficients 
     :param b: An array of b hybrid level coefficients 
 
-
-    :return: Returns a list of Python datetimes.
+    :return: Returns  4-D array of grid-cell-center pressure.
     """
 
     # if ps is 2-D, make it 3-D
@@ -43,3 +47,80 @@ def calc_pressure_cesm(ps: np.ndarray, p0: float, a: np.array, b: np.array) -> l
     p = p[:,::-1,:,:] # flip level index so that pressures are decreasing with level, height
 
     return p
+
+def calc_phii_midpoint(z: np.ndarray, zs: np.ndarray) -> np.ndarray:
+    """
+    calc_phii_midpoint estimates the vertical interfaces of grid cells via midpoint
+
+    :param z: A 4-D (ntime by nlev by nlat by nlon) numpy 
+              array grid-cell-center geopotential height in meters
+    :param zs: a 2-D array of the geopotential height of the surface in meters
+
+    :return: Returns a 4-D numpy array of geopotential height at the interfaces.
+    """
+
+    nt = z.shape[0]
+    nz = z.shape[1]
+    nlat = z.shape[2]
+    nlon = z.shape[3]
+
+    zi = np.zeros((nt,nz+1,nlat,nlon)) # if 3 levels were provided, want to return a 4-level interface height
+    zi[:,0,:,:] = zs # set the surface, 0 index
+    # for the, say, 3 level z's provided, compute the 2 midpoints
+    zi[:,1:nz,:,:] = 0.5*(z[:,1:nz,:,:]+z[:,0:nz-1,:,:])
+    # now, estimate the model top height: ztop = z[-1] + (z[-1] - 0.5*(z[-1] + z[-2]))
+    zi[:,nz,:,:] = z[:,-1,:,:] + z[:,-1,:,:] - zi[:,nz-1,:,:]
+    return zi
+
+def calc_t_virt(tk: np.ndarray, q: np.ndarray) -> np.ndarray:
+    """
+    calc_t_virt calculates the virtual temperature given temperature and specific humidity
+
+    :param tk: a N-D array of temperature in Kelvins
+    :param q: a N-D array of specific humidity (rhov/rho), non-dimensional
+
+    :return: Returns an N-D numpy array of density of moist air
+    """
+    factor = 1./ep - 1
+    tv = tk*(1 + q * factor)
+    return tv
+
+def calc_rhom(p: np.ndarray, tk: np.ndarray, q: np.ndarray) -> np.ndarray:
+    """
+    calc_rhom calculates the density of moist air given pressure, temperature, and specific humidity
+
+    :param p: a N-D array of pressure in Pa
+    :param tk: a N-D array of temperature in Kelvins
+    :param q: a N-D array of specific humidity (rhov/rho), non-dimensional
+
+    :return: Returns an N-D numpy array of density of moist air
+    """
+    rhom = p/Rd/calc_t_virt(tk,q)
+    return rhom
+
+def calc_phii_hydro(zs: np.ndarray, rhom: np.ndarray, pi: np.ndarray) -> np.ndarray:
+    """
+    calc_phii_hydro estimates the vertical interfaces of grid cells via integration on the hydrostatic
+                    equation.
+
+    :param zs: a 2-D array of the geopotential height of the surface in meters
+    :param rhom: a 4-D array of density of the air (e.g. moist air)
+    :param pi: a 4-D array of pressure at the vertical interfaces grid-cell interfaces
+
+    :return: Returns a 4-D numpy array of geopotential height at the interfaces.
+    """
+
+    nt = rhom.shape[0]
+    nz = rhom.shape[1] # number of model levels, not interfaces
+    nlat = rhom.shape[2]
+    nlon = rhom.shape[3]
+
+    zi = np.zeros((nt,nz+1,nlat,nlon)) # if 3 levels were provided, want to return a 4-level interface height
+    zi[:,0,:,:] = zs # set the surface, 0 index
+    for i in range(nz):
+        dp = pi[:,i+1,:,:] - pi[:,i,:,:]
+        dz = -dp/rhom[:,i,:,:]/g
+        zi[:,i+1,:,:] = zi[:,i,:,:] + dz
+    return zi
+
+
